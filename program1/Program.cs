f5e085bab113fdc6c02d115633e8c522948ef692
akspirial <bulatvaleksejjj@gmail.com>
2023-09-14 03:28:17 +0300

    seminar1

---
+++ program1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите число a: ");
+	int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+	int b = int.Parse(Console.ReadLine());
+	if (a > b)
+	{ 
+		Console.Write("Большее число : ");
+		Console.WriteLine(a);
+		Console.Write("Меньшее число : ");
+		Console.WriteLine(b);
+			
+	}
+	else if (b > a)
+	{
+	     Console.Write("Большее число : ");
+		 Console.WriteLine(b);
+		 Console.Write("Меньшее число : ");
+		 Console.WriteLine(a);
+	}
+	else if (b == a)
+	{
+	     Console.WriteLine("Числа равны");
+			
+	}
---
+++ program2/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введите число а: ");
+	int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+	int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+	int c = int.Parse(Console.ReadLine());
+	if (a > b && a > c)
+	{ 
+		Console.Write(" Самое большее число : ");
+		Console.WriteLine(a);
+		
+	}
+	else if (b > a && b > c )
+	{
+         Console.Write("Самое большее число : ");
+		 Console.WriteLine(b);
+	
+	}
+	else if (c > a && c > b )
+	{
+        Console.Write("Самое большее число : ");
+		Console.WriteLine(c);
+		
+	}
+	else if (c == a  && c == b)
+	{
+		Console.Write("Ваши числа равны ");
+	}
