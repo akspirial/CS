81bde47584dffc464791997275cc5cfacb46f7ea
akspirial <bulatvaleksejjj@gmail.com>
2023-10-06 01:50:12 +0300

    seminar9

---
+++ seminar9.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Prog
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbers(n);
+
+        Console.ReadKey();
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n >= 1)
+        {
+            Console.WriteLine(n);
+            PrintNumbers(n - 1);
+        }
+    }
+}
---
+++ seminar9.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Prog
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = SumNumbers(m, n);
+
+        Console.WriteLine("Сумма натуральных элементов в промежутке от {0} до {1} равна {2}", m, n, sum);
+
+        Console.ReadKey();
+    }
+
+    static int SumNumbers(int m, int n)
+    {
+        if (m > n)
+        {
+            return 0;
+        }
+        else
+        {
+            return m + SumNumbers(m + 1, n);
+        }
+    }
+}
