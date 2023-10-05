f49ad36c09f9ad3fb04df19f34a0c93a41c06ee0
akspirial <bulatvaleksejjj@gmail.com>
2023-10-06 01:51:04 +0300

    seminar9

--- seminar9.3/Program.cs
+++ seminar9.3/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+class Prog
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение m: ");
+        int m = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение n: ");
+        int n = int.Parse(Console.ReadLine());
+        int result = Akkerman(m, n);
+        Console.WriteLine("Значение функции Аккермана для m={0} и n={1} равно {2}", m, n, result);
+
+        Console.ReadKey();
+    }
+
+    static int Akkerman(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
