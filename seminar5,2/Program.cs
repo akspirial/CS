b27d3b77958bd2d405fa7a5dff51862b7ecb9cff
akspirial <bulatvaleksejjj@gmail.com>
2023-10-06 00:50:35 +0300

    seminar_5

---
+++ seminar5,2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите числа, разделенные пробелами:");
+        string input = Console.ReadLine();
+
+        string[] numbersString = input.Split(' '); 
+        int[] numbers = new int[numbersString.Length];
+
+        for (int i = 0; i < numbersString.Length; i++)
+        {
+            numbers[i] = int.Parse(numbersString[i]); 
+        }
+
+        int sum = 0;
+        for (int i = 1; i < numbers.Length; i += 2)
+        {
+            sum += numbers[i];
+        }
+
+        Console.WriteLine("Сумма элементов на нечетных индексах: " + sum);
+    }
+}
---
+++ seminar5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        int size = 10;
+
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(100, 1000);
+        }
+
+        int count = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine($"Количество четных чисел в массиве: {count}");
+    }
+}
